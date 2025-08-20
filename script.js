0ffba2255a6b65d1ba4b196f64d2b05e4957e06d
Pedro Victor <swagpeh@gmail.com>
2025-08-19 22:12:08 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+
+  /*pegar a tag image*/
+  const image= document.querySelector("#profile img")
+  
+  /*substituir a imagem*/
+
+  
+}
